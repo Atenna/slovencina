a8a507d266524be25c2d10300c23b7ec6bb5758f
Ivana Lieskovska <idrabikova@gmail.com>
2015-12-30 22:20:01 +0100

    Static Pages Controllers
    Routes, Desing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Slovencina::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
   devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+
+
 
   resources :graphs
   resources :attempts
@@ -12,5 +18,6 @@ Slovencina::Application.routes.draw do
   
   match '/:controller(/:action(/:id))'
 
-  root :to => 'statistics#index'
+  #root :to => 'statistics#index'
+  root :to => 'static_pages#home'
 end
