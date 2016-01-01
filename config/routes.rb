927da57c9b654d7ea879563708a26456ea5bb2eb
Ivana Lieskovska <idrabikova@gmail.com>
2016-01-01 19:43:43 +0100

    Revert "Static Pages Controllers"
    This reverts commit a8a507d266524be25c2d10300c23b7ec6bb5758f.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Slovencina::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/about"
-
   devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
-
-
 
   resources :graphs
   resources :attempts
@@ -18,6 +12,5 @@ Slovencina::Application.routes.draw do
   
   match '/:controller(/:action(/:id))'
 
-  #root :to => 'statistics#index'
-  root :to => 'static_pages#home'
+  root :to => 'statistics#index'
 end
